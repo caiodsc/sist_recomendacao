166ec6147f2b2224359b181413d2dcf0b999be5e
Caio <caio.dscamara@gmail.com>
2017-11-27 00:33:25 -0200

    Movielens algoritmo completo.

--- recomendacao_movielens.py
+++ recomendacao_movielens.py
@@ -1,3 +1,44 @@
+from math import sqrt
+from base_de_dados import avaliacoes
+def euclidiana (base, usuario1, usuario2):
+    i = False
+    for item in base[usuario1]:
+        if item in base[usuario2]:
+            i = True
+            break
+    if not i:
+        return 0
+    r = sqrt(sum([pow(base[usuario1][item] - base[usuario2][item], 2) for item in base[usuario1] if item in base[usuario2]]))
+    return (1/(1+r))
+    #c = sqrt(sum((avaliacoes[usuario1].get(d, 0) - avaliacoes[usuario2].get(d, 0)) ** 2 for d in set(avaliacoes[usuario1]) & set(avaliacoes[usuario2])))
+    #print (1/(1+c))
+
+def getSimilares(base, usuario):
+    similaridade = [(euclidiana(base, usuario, outro), outro) for outro in base if outro != usuario]
+    similaridade.sort()
+    similaridade.reverse()
+    return similaridade
+
+def getRecomendacoes(base, usuario):
+    totais = {}
+    somaSimilaridade = {}
+    for outro in base:
+        if outro == usuario: continue
+        similaridade = euclidiana(base, usuario, outro)
+        if similaridade <= 0: continue
+        for item in base[outro]:
+            #### somente o que o usuário alvo ainda não avaliou
+            if item not in base[usuario]:
+                totais.setdefault(item, 0)
+                totais[item] += base[outro][item] * similaridade
+                somaSimilaridade.setdefault(item, 0)
+                somaSimilaridade[item] += similaridade
+    rankings = [(total/somaSimilaridade[item], item) for item, total in totais.items()]
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+
 def carregaMovieLens():
     filmes = {}
     for linha in open('./u.item'):
@@ -9,6 +50,11 @@ def carregaMovieLens():
         (usuario, idfilme, nota, tempo) = linha.split('\t')
         base.setdefault(usuario, {})
         base[usuario][filmes[idfilme]] = float(nota)
-    print(base)
+    return base
 
-carregaMovieLens()
+print(getSimilares(avaliacoes, 'Pedro'))
+
+
+
+
+
